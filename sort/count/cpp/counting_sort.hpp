d44bb4fd8cbb062b3e925835ed516c9b780800fa
Zhifeng Sun <austin.zhifeng.sun@gmail.com>
2015-02-07 15:10:50 -0800

    counting sort and radix sort c++ imp.

---
+++ sort/count/cpp/counting_sort.hpp
@@ -0,0 +1,16 @@
+#include <vector>
+
+// KeyFn is a functor that maps T to int.
+// 'k' specifies the range of key_fn(T). [0...k].
+template<typename T, typename KeyFn>
+void counting_sort(const std::vector<T>& A, std::vector<T>& B,
+                   size_t k, KeyFn key_fn) {
+  B.resize(A.size());
+  std::vector<int> C(k+1, 0);
+  for (const T& a : A) C[key_fn(a)]++;
+  for (int i = 1; i <= k; i++) C[i] += C[i-1];
+  for (int i = A.size()-1; i >= 0; i--) {
+    int pos = C[key_fn(A[i])]-- - 1;
+    B[pos] = A[i];
+  }
+}
---
+++ sort/count/cpp/test.cpp
@@ -0,0 +1,14 @@
+#include <vector>
+#include "gmock/gmock.h"
+
+#include "counting_sort.hpp"
+
+using namespace std;
+using namespace testing;
+
+TEST(QuicksortTest, GeneralCase) {
+  vector<int> A = {3,1,2,5,3,1,5,2,4};
+  vector<int> B;
+  counting_sort(A, B, 9, [](int a) -> int{ return a; });
+  EXPECT_THAT(B, ElementsAre(1,1,2,2,3,3,4,5,5));
+}
