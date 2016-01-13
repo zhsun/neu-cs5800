a27ce4f84cc853fa75f41388093d56291208e0ef
Zhifeng Sun <austin.zhifeng.sun@gmail.com>
2016-01-12 21:41:28 -0800

    reorg recursion dir

--- recursion/combination/py/comb.py
+++
@@ -1,9 +0,0 @@
-def combination(A):
-    if not A: return [[]]
-    first = A[0]
-    remain = A[1:]
-    remain_comb = combination(remain)
-    addition_comb = []
-    for comb in remain_comb:
-        addition_comb.append([first] + comb)
-    return remain_comb + addition_comb
